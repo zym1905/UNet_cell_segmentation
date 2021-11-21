167fbd05f947c4cfe20eb23bd877a1e7bd267c3c
Yong Zhang <zhangyong2@genomics.cn>
2021-11-21 23:24:43 +0800

    Update uNetInference.py

--- uNetInference.py
+++ uNetInference.py
@@ -5,7 +5,10 @@ import os
 import numpy as np
 import torch
 import torch.nn.functional as F
+
 from PIL import Image
+Image.MAX_IMAGE_PIXELS = None
+
 from torchvision import transforms
 
 from uNetData import BasicDataset
@@ -49,13 +52,13 @@ def get_args():
     parser.add_argument('--model', '-m', default='MODEL.pth', metavar='FILE',
                         help='Specify the file in which the model is stored')
     parser.add_argument('--input', '-i', metavar='INPUT', nargs='+', help='Filenames of input images', required=True)
-    parser.add_argument('--output', '-o', metavar='INPUT', nargs='+', help='Filenames of output images')
+    parser.add_argument('--output', '-o', default='./', metavar='OUTPUT', help='dir for output images')
     parser.add_argument('--viz', '-v', action='store_true',
                         help='Visualize the images as they are processed')
     parser.add_argument('--no-save', '-n', action='store_true', help='Do not save the output masks')
     parser.add_argument('--mask-threshold', '-t', type=float, default=0.5,
                         help='Minimum probability value to consider a mask pixel white')
-    parser.add_argument('--scale', '-s', type=float, default=0.5,
+    parser.add_argument('--scale', '-s', type=float, default=1,
                         help='Scale factor for the input images')
 
     return parser.parse_args()
@@ -75,13 +78,55 @@ def mask_to_image(mask: np.ndarray):
     elif mask.ndim == 3:
         return Image.fromarray((np.argmax(mask, axis=0) * 255 / mask.shape[0]).astype(np.uint8))
 
+image_size = ()
+def cutImage(tiff_image, outdir):
+    #tiff_image = '/Users/zhangyong/work/MLIM/data/20210426-T215-Z3-L-M019-01_regist.tif'
+    #outdir = '/Users/zhangyong/work/MLIM/data/slices'
+
+    image = Image.open(tiff_image)
+    width, height = image.size
+    global image_size
+    image_size = image.size
+
+    files = []
+    step_size = 256
+    upper = 0
+    left = 0
+    right = step_size
+    lower = step_size
+    count = 0
+
+    while lower < height:
+        bbox = (left, upper, right, lower)
+        image_slice = image.crop(bbox)
+        path = os.path.join(outdir, "slice_20210426-T215-Z3-L-M019-01_regist." + str(left) + "_" + str(upper) + ".png")
+        files.__add__(path)
+        image_slice = image_slice.convert('L')
+        image_slice.save(path)
+
+        if right > width:
+            upper += step_size
+            lower += step_size
+            left = 0
+            right = step_size
+        else:
+            left += step_size
+            right += step_size
+
+        count += 1
+
+    print('split image into:' + str(count) + ' iamges.')
+    return files
+
 
 if __name__ == '__main__':
     args = get_args()
     in_files = args.input
-    out_files = get_output_filenames(args)
+    #out_files = get_output_filenames(args)
+    outdir = args.output
+    global image_size
 
-    net = UNet(n_channels=3, n_classes=2)
+    net = UNet(n_channels=1, n_classes=2)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     logging.info(f'Loading model {args.model}')
@@ -92,6 +137,30 @@ if __name__ == '__main__':
 
     logging.info('Model loaded!')
 
+    for i, filename in enumerate(in_files):
+        new_iamge = Image.new('L', image_size)
+        step_size = 256
+        count = 0
+        for slices in cutImage(filename, outdir):
+            logging.info(f'\nPredicting image {filename} ...')
+            img = Image.open(slices)
+            mask = predict_img(net=net,
+                               full_img=img,
+                               scale_factor=args.scale,
+                               out_threshold=args.mask_threshold,
+                               device=device)
+            str_tmp = os.path.splitext(os.path.splitext(slices))
+            str_list = str_tmp[1].split('_')
+            left = int(str_list[0])
+            upper = int(str_list[1])
+            right = left + step_size
+            lower = upper + step_size
+            bbox = (left, upper, right, lower)
+            new_iamge.paste(mask, bbox)
+
+        new_iamge.save(os.path.join(str_tmp[0] + '.png'))
+
+'''
     for i, filename in enumerate(in_files):
         logging.info(f'\nPredicting image {filename} ...')
         img = Image.open(filename)
@@ -111,3 +180,4 @@ if __name__ == '__main__':
         if args.viz:
             logging.info(f'Visualizing results for image {filename}, close to continue...')
             #plot_img_and_mask(img, mask)
+'''
